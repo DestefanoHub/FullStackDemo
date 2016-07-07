ffdd3c8da4e6a61a90bdc0849c877dbdacf73076
andrew <elitehunter92@gmail.com>
2016-07-07 12:45:33 -0400

    Added: - all battlemech intrinsic properties

--- src/AppBundle/Entity/BattleMech.php
+++ src/AppBundle/Entity/BattleMech.php
@@ -84,4 +84,118 @@ class BattleMech
      * @JMS\Expose()
      */
     protected $tonnage;
+
+    /**
+     * @var double
+     * @ORM\Column(name="speed", type="decimal", precision=4, scale=2)
+     * @Assert\NotBlank(
+     *      message="A BattleMech's speed cannot be blank."
+     * )
+     *
+     * @JMS\Expose()
+     */
+    protected $speed;
+
+    /**
+     * @var string
+     * @ORM\Column(name="engine_type", type="string", length=10)
+     * @Assert\NotBlank(
+     *      message="A BattleMech's engine type cannot be blank."
+     * )
+     * @Assert\Length(
+     *      max=10,
+     *      maxMessage="A Battlemech's engine type must be less than {{ limit }} characters."
+     * )
+     *
+     * @JMS\Expose()
+     */
+    protected $engineType;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="engine_rating", type="integer")
+     * @Assert\NotBlank(
+     *      message="A BattleMech's engine rating cannot be blank."
+     * )
+     * @Assert\Range(
+     *      min=60,
+     *      max=400,
+     *      minMessage="A BattleMech's engine rating cannot be less than {{ limit }}.",
+     *      maxMessage="A BattleMech's engine rating cannot be more than {{ limit }}."
+     * )
+     *
+     * @JMS\Expose()
+     */
+    protected $engineRating;
+
+    /**
+     * @var string
+     * @ORM\Column(name="tech_base", type="string", length=20)
+     * @Assert\NotBlank(
+     *      message="A BattleMech's technology base cannot be blank."
+     * )
+     * @Assert\Length(
+     *      max=20,
+     *      maxMessage="A Battlemech's technology base must be less than {{ limit }} characters."
+     * )
+     * @JMS\Expose()
+     */
+    protected $techBase;
+
+    /**
+     * @var string
+     * @ORM\Column(name="internal_structure_type", type="string", length=20)
+     * @Assert\NotBlank(
+     *      message="A BattleMech's internal structure type cannot be blank."
+     * )
+     * @Assert\Length(
+     *      max=20,
+     *      maxMessage="A Battlemech's internal structure type must be less than {{ limit }} characters."
+     * )
+     *
+     * @JMS\Expose()
+     */
+    protected $internalStructureType;
+
+    /**
+     * @var string
+     * @ORM\Column(name="armor_base", type="string", length=20)
+     * @Assert\NotBlank(
+     *      message="A BattleMech's armor type cannot be blank."
+     * )
+     * @Assert\Length(
+     *      max=20,
+     *      maxMessage="A Battlemech's armor type must be less than {{ limit }} characters."
+     * )
+     *
+     * @JMS\Expose()
+     */
+    protected $armorType;
+
+    /**
+     * @var string
+     * @ORM\Column(name="heatsink_type", type="string", length=10)
+     * @Assert\NotBlank(
+     *      message="A BattleMech's heatsink type cannot be blank."
+     * )
+     * @Assert\Length(
+     *      max=10,
+     *      maxMessage="A Battlemech's heatsink type must be less than {{ limit }} characters."
+     * )
+     *
+     * @JMS\Expose()
+     */
+    protected $heatSinkType;
+
+    /**
+     * @var string
+     * @ORM\Column(name="image", type="string", nullable=true)
+     *
+     * @JMS\Expose()
+     */
+    protected $image;
+
+//    protected $weapons;
+//
+//    protected $equipment;
 }
